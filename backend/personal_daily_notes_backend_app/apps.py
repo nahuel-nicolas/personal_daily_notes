ab0023aa3b0dcf9f1a8839f0506e32db809bd4e9
nahuel-nicholas <nahuel012@gmail.com>
2022-06-01 16:02:30 -0300

    fixed wrong add

--- backend/personal_daily_notes_backend_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PersonalDailyNotesBackendAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'personal_daily_notes_backend_app'
